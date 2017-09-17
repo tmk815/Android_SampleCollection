7357da87e11e8da1e5ba5363e6a81460518b7d3d
門田朋己 <monndenn15@gmail.com>
2017-09-17 16:19:48 +0900

    RelativeLayoutを作成

--- java/com/example/mondentomoki/android_samplecollection/MainActivity.java
+++ java/com/example/mondentomoki/android_samplecollection/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.mondentomoki.android_samplecollection;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(MainActivity.this, RelativeLayout.class));
+                        break;
+                }
+            }
+        });
     }
 }
---
+++ java/com/example/mondentomoki/android_samplecollection/RelativeLayout.java
@@ -0,0 +1,17 @@
+package com.example.mondentomoki.android_samplecollection;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MondenTomoki on 2017/09/17.
+ */
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
