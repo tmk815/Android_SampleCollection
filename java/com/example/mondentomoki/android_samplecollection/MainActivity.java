58e8f8f531961bda690837e4f174e74dbf8b33b9
門田朋己 <monndenn15@gmail.com>
2017-09-17 15:38:53 +0900

    プロジェクトを作成

---
+++ java/com/example/mondentomoki/android_samplecollection/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mondentomoki.android_samplecollection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
